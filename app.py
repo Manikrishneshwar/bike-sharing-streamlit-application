dc064c66d095a543e44eea340e63a266d21923b3
inam <manikrish2003@gmail.com>
2025-11-27 15:09:13 -0500

    testing streamlit code to display some metrics

--- app.py
+++ app.py
@@ -10,3 +10,20 @@ latlon_url='https://tor.publicbikesystem.net/ube/gbfs/v1/en/station_information'
 data_df=query_station_status(station_url)
 latlon_df=get_station_latlon(latlon_url)
 data=join_latlon(data_df,latlon_df)
+
+# Stream lit application code starts here
+st.title('Torronto Bike Share Station Status')
+st.markdown('This dashboard tracks bike availability at each bike share station in Torronto.')
+
+# We create columns for the dashboard in order to display some metrics.
+# the metrics displayed are number of availability of bikes, and number 
+# of stations with availability of bikes and docks.
+col1,col2,col3=st.columns(3)
+with col1:
+    st.metric(label='Bikes Available Now',value=sum(data['num_bikes_available']))
+    st.metric(label='EBikes Available Now',value=sum(data['ebike']))
+with col2:
+    st.metric(label='Stations w Available Bikes',value=len(data[data['num_bikes_available']>0]))
+    st.metric(label='Stations w Available E-bikes',value=len(data[data['ebike']>0]))
+with col3:
+    st.metric(label='Stations w Empty Docks',value=len(data[data['num_docks_available']>0]))
